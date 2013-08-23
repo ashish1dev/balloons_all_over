bb2f9f1587f33eb2e5bcbbb8422e18bc5fe65498
Ashish <ashish.fagna@gmail.com>
2013-08-23 23:31:27 +0530

    animated balloons

---
+++ balloons_all_over.js
@@ -0,0 +1,63 @@
+
+
+$(window).load(function(){
+
+var host="http://tettra.in/balloons_all_over/";
+
+$("#balloons_all_over").html("<img src='"+host+"balloons/blue.png' style='position:absolute;' id='_1' alt='_1'/><img src='"+host+"balloons/green.png' style='position:absolute;' id='_2' alt='_2'/><img src='"+host+"balloons/pink.png' style='position:absolute;' id='_3' alt='_3'/><img src='"+host+"balloons/red.png' style='position:absolute;' id='_4' alt='_4'/><img src='"+host+"balloons/yellow.png' style='position:absolute;' id='_5' alt='_5'/><img src='"+host+"balloons/green_light.png' style='position:absolute;' id='_6' alt='_6'/><img src='"+host+"balloons/blue_light.png' style='position:absolute;' id='_7' alt='_7'/><img src='"+host+"balloons/blue.png' style='position:absolute;' id='_8' alt='_8'/><img src='"+host+"balloons/green.png' style='position:absolute;' id='_9' alt='_9'/><img src='"+host+"balloons/pink.png' style='position:absolute;' id='_10' alt='_10'/><img src='"+host+"balloons/red.png' style='position:absolute;' id='_11' alt='_11'/><img src='"+host+"balloons/yellow.png' style='position:absolute;' id='_12' alt='_12'/><img src='"+host+"balloons/green_light.png' style='position:absolute;' id='_13' alt='_13'/><img src='"+host+"balloons/blue_light.png' style='position:absolute;' id='_14' alt='_14'/>");
+
+	var top_position=200;
+	var left_position=10;
+
+	var h_direction="";
+
+
+    setPosition("_1",h_direction,top_position,left_position,0,500,7,110);
+	setPosition("_2",h_direction,top_position+40,left_position+130,140,600,8,110);
+	setPosition("_3",h_direction,top_position+580,left_position+760, 0,600,7,100);
+	setPosition("_4",h_direction,top_position+620,left_position+180,340,960,8,100);
+	setPosition("_5",h_direction,top_position+160,left_position+50, 50,789,8,100);
+	setPosition("_6",h_direction,top_position+820,left_position+420,400,1200,7,100);
+	setPosition("_7",h_direction,top_position+930,left_position+810, 0,700,8,110);
+	
+	setPosition("_8",h_direction,top_position+370,left_position+670, 0,1120,7,110);
+	setPosition("_9",h_direction,top_position+110,left_position+620,50,1000,8,110);
+	setPosition("_10",h_direction,top_position+260,left_position+370,70,900,8,110);
+	setPosition("_11",h_direction,top_position-780,left_position+660,80,1000,7,110);
+	setPosition("_12",h_direction,top_position+310,left_position+485,100,1200,7,110);
+	setPosition("_13",h_direction,top_position+680,left_position, 0,1600,7,110);
+	setPosition("_14",h_direction,top_position+400,left_position+40,-10,750,7,110);
+	
+	
+});
+
+var document_height=$(document).height();
+
+
+
+function setPosition(id,h_direction,top_position,left_position,left_range,right_range,delta,delay){
+	$("#"+id).css("top", top_position);
+	$("#"+id).css("left",left_position);
+
+	top_position=top_position-delta;
+	
+	if(left_position>right_range)
+		h_direction="left";
+	else if(left_position<left_range)
+		h_direction="right";
+	
+	if(h_direction=="left")
+		left_position=left_position-delta;
+	else
+		left_position=left_position+delta;
+	
+	if(top_position<-290)
+		top_position=document_height;
+		
+	
+
+
+		setTimeout(	function() { 	
+			setPosition(id,h_direction,top_position,left_position,left_range,right_range,delta,delay);	
+		},delay); 
+	}
---
+++ index.php
@@ -0,0 +1,96 @@
+<html xmlns:fb="http://ogp.me/ns/fb#">
+<head>
+
+<title>Ballons All over | Tettra.in | Decorative Mashup</title>
+
+</head>
+<body>
+
+
+<div id="fb-root"></div>
+<script>(function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) return;
+  js = d.createElement(s); js.id = id;
+  js.src = "//connect.facebook.net/en_US/all.js#xfbml=1";
+  fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));</script>
+
+
+<div id="balloons_all_over"></div>
+<script type="text/javascript" src="http://tettra.in/balloons_all_over/jquery-1.7.1.min.js"></script>
+<script type="text/javascript" src="http://tettra.in/balloons_all_over/balloons_all_over.js"></script>
+
+
+
+    <!-- Le styles -->
+    <link href="http://tettra.in/balloons_all_over/bootstrap/css/bootstrap.css" rel="stylesheet">
+    <link href="http://tettra.in/balloons_all_over/bootstrap/css/bootstrap-responsive.css" rel="stylesheet">
+	<script type="text/javascript" src="http://tettra.in/balloons_all_over/bootstrap-modal.js"></script>
+	<script type="text/javascript" src="http://tettra.in/balloons_all_over/bootstrap-modalmanager.js"></script>
+	
+    <!-- HTML5 shim, for IE6-8 support of HTML5 elements -->
+    <!--[if lt IE 9]>
+      <script src="bootstrap/js/html5shiv.js"></script>
+    <![endif]-->
+
+	<style>
+	#myModal{
+		position:absolute;
+		top:40%;
+		width:60%;
+	}
+	#fb_div{
+		position:absolute;
+		top:25%;
+		width:60%;
+		left:40%;
+	}
+
+	#launchButton{
+		position:absolute;
+		left:40%;
+		top:10%;
+	}
+	</style>
+    <!-- Button to trigger modal -->
+    <a href="#myModal" role="button" class="btn" id="launchButton" data-toggle="modal">Add these Balloons to your webpage.</a>
+     
+	 
+	
+	
+    <!-- Modal -->
+    <div id="myModal" class="modal hide fade" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+
+    <div class="modal-header">
+    <button type="button" class="close" data-dismiss="modal" aria-hidden="true">X</button>
+    <h3 id="myModalLabel">To Add Balloons,Just embed this code into your webpage.</h3>
+	
+	
+    </div>
+    <div class="modal-body">
+    <p>
+	
+		&lt;script type='text/javascript' src='http://tettra.in/balloons_all_over/jquery-1.7.1.min.js'&gt;&lt;/script&gt;<br />
+		&lt;script type='text/javascript' src='http://tettra.in/balloons_all_over/balloons_all_over.js'&gt;&lt;/script&gt;<br />
+		&lt;div id='balloons_all_over'&gt;&lt;/div&gt;
+		
+	</p>
+    </div>
+    <div class="modal-footer">
+    <button class="btn" data-dismiss="modal" aria-hidden="true">Close</button>
+    </div>
+    </div>
+	
+	<div id="fb_div">
+		
+		<a href="https://twitter.com/share" class="twitter-share-button" data-count="none">Tweet</a>
+<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');</script>
+
+<fb:like href="http://tettra.in/balloons_all_over/" send="true" width="450" show_faces="true" font="arial"></fb:like>
+		
+	</div>
+	
+	
+</body>
+</html>
